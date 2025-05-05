0c3e1e79e5ef71eefc8a0448c3d6b10bdb026ac7
Ruba <91896958+RubaUser@users.noreply.github.com>
2025-05-05 21:18:39 +0300

    function to get all todos

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index() {
+        $todos = auth()->user()->todos()->get();
+        return response()->json([
+            'todos' => $todos
+        ]);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -23,6 +23,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function todos() {
+        return $this->hasMany(Todo::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
