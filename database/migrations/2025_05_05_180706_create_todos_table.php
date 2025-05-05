4414301dabba947b4059bfa1727e88b7f8c9f86f
Ruba <91896958+RubaUser@users.noreply.github.com>
2025-05-05 21:14:09 +0300

    todo migration

--- database/migrations/2025_05_05_180706_create_todos_table.php
+++ database/migrations/2025_05_05_180706_create_todos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('todos', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->boolean('completed')->default(false);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
